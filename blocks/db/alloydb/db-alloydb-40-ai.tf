bef2a3b2140333a5f2247c6036a3877acdc2f0c6
J.K. Kashe <jkashe@google.com>
2024-11-22 11:58:53 +0100

    fix for alloydb.ai

--- blocks/db/alloydb/db-alloydb-40-ai.tf
+++ blocks/db/alloydb/db-alloydb-40-ai.tf
@@ -1,3 +1,17 @@
+locals {
+  alloydb_sa_roles = [
+    "roles/aiplatform.user"
+  ]
+}
+
+resource "google_project_iam_member" "alloydb_sa_roles" {
+  for_each = toset(local.alloydb_sa_roles)
+  project  = local.project_id
+  role     = each.key
+  member   = "serviceAccount:service-${local.project_number}@gcp-sa-alloydb.iam.gserviceaccount.com"
+  depends_on = [google_alloydb_instance.primary_instance]
+}
+
 resource "null_resource" "db-alloydb-ai-" {
   depends_on = [null_resource.alloydb_pgauth,
                 null_resource.install_postgresql_client]
