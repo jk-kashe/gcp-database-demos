4b98a2c5dba8f4a2df3ac1d3c537c890d41e1cb5
J.K. Kashe <jkashe@google.com>
2024-11-25 10:21:31 +0100

    fixed duplicated resource

--- blocks/0_landing_zone/05-landing-zone-existing-project.tf
+++ blocks/0_landing_zone/05-landing-zone-existing-project.tf
@@ -16,10 +16,6 @@ data "google_project" "project_check" {
   depends_on = [google_project_service.project_rm_api]
 }
 
-data "google_project" "project_check" {
-  project_id = var.demo_project_id
-}
-
 locals {
   project_id     = data.google_project.project_check.project_id
   project_name   = data.google_project.project_check.name
