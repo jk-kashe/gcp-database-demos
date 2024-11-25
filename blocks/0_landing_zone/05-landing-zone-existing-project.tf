1e44d9effbf87532e62f07545dd40faf658faec8
J.K. Kashe <jkashe@google.com>
2024-11-25 10:17:22 +0100

    trying to enable some apis before we fetch project

--- blocks/0_landing_zone/05-landing-zone-existing-project.tf
+++ blocks/0_landing_zone/05-landing-zone-existing-project.tf
@@ -1,3 +1,21 @@
+resource "null_resource" "enable_service_usage_api_pre_proj" {
+  provisioner "local-exec" {
+    command = "gcloud services enable serviceusage.googleapis.com --project ${var.demo_project_id}"
+  }
+}
+
+resource "google_project_service" "project_rm_api" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+  depends_on         = [null_resource.enable_service_usage_api_pre_proj]
+  project            = var.demo_project_id
+}
+
+data "google_project" "project_check" {
+  project_id = var.demo_project_id
+  depends_on = [google_project_service.project_rm_api]
+}
+
 data "google_project" "project_check" {
   project_id = var.demo_project_id
 }
