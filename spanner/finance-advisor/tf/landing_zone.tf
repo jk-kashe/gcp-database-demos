53d4566ab082c5ea14a5dfefd336cd2fdf1b4304
J.K. Kashe <jkashe@google.com>
2025-03-14 09:41:35 +0100

    updated/built after latest PR

--- spanner/finance-advisor/tf/landing_zone.tf
+++ spanner/finance-advisor/tf/landing_zone.tf
@@ -72,73 +72,6 @@ locals {
   project_name   = var.create_new_project ? google_project.demo-project[0].name : data.google_project.project_check[0].name
   project_number = var.create_new_project ? google_project.demo-project[0].number : data.google_project.project_check[0].number
 }
-# Network Resources
-resource "google_compute_network" "demo_network" {
-  name                    = "demo-network"
-  auto_create_subnetworks = true
-  depends_on              = [google_project_service.project_services]
-  project                 = local.project_id
-
-}
-
-# Enable PGA
-resource "null_resource" "demo_network_pga" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      gcloud compute networks subnets update ${google_compute_network.demo_network.name} \
-        --project=${local.project_id} \
-        --region=${var.region} \
-        --enable-private-ip-google-access
-    EOT
-  }
-}
-
-resource "google_compute_global_address" "psa_range" {
-  name          = "psa-range"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 24
-  network       = google_compute_network.demo_network.id # Or your custom network
-  project       = local.project_id
-}
-
-resource "google_service_networking_connection" "private_service_access" {
-  network                 = google_compute_network.demo_network.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.psa_range.name]
-}
-
-resource "google_compute_firewall" "allow_iap_ssh" {
-  name      = "allow-iap-ssh"
-  network   = google_compute_network.demo_network.id
-  direction = "INGRESS"
-  project   = local.project_id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["35.235.240.0/20"]
-}
-
-# Create a NAT gateway
-resource "google_compute_router" "nat-router" {
-  name    = "nat-router"
-  region  = var.region
-  network = google_compute_network.demo_network.name
-  project = local.project_id
-}
-
-resource "google_compute_router_nat" "nat-config" {
-  name                               = "nat-config"
-  router                             = google_compute_router.nat-router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  project                            = local.project_id
-}
-
 resource "null_resource" "enable_service_usage_api" {
   provisioner "local-exec" {
     command = "gcloud services enable serviceusage.googleapis.com --project ${local.project_id}"
@@ -234,6 +167,73 @@ resource "time_sleep" "wait_for_database_clientvm_boot" {
   depends_on = [google_compute_instance.database-clientvm,
   null_resource.lz-init-gcloud-ssh]
 }
+# Network Resources
+resource "google_compute_network" "demo_network" {
+  name                    = "demo-network"
+  auto_create_subnetworks = true
+  depends_on              = [google_project_service.project_services]
+  project                 = local.project_id
+
+}
+
+# Enable PGA
+resource "null_resource" "demo_network_pga" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      gcloud compute networks subnets update ${google_compute_network.demo_network.name} \
+        --project=${local.project_id} \
+        --region=${var.region} \
+        --enable-private-ip-google-access
+    EOT
+  }
+}
+
+resource "google_compute_global_address" "psa_range" {
+  name          = "psa-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.demo_network.id # Or your custom network
+  project       = local.project_id
+}
+
+resource "google_service_networking_connection" "private_service_access" {
+  network                 = google_compute_network.demo_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.psa_range.name]
+}
+
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name      = "allow-iap-ssh"
+  network   = google_compute_network.demo_network.id
+  direction = "INGRESS"
+  project   = local.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
+# Create a NAT gateway
+resource "google_compute_router" "nat-router" {
+  name    = "nat-router"
+  region  = var.region
+  network = google_compute_network.demo_network.name
+  project = local.project_id
+}
+
+resource "google_compute_router_nat" "nat-config" {
+  name                               = "nat-config"
+  router                             = google_compute_router.nat-router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  project                            = local.project_id
+}
+
 
 resource "google_project_service" "lz_dataflow_service" {
   service    = "dataflow.googleapis.com"
