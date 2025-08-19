69ea30ea633701e174c7b20f8ac5951e9ed31eea
Tom Bottrill <tombot@google.com>
2025-08-19 09:36:53 +0000

    Adds IAP to Cloud Run

--- spanner/finance-advisor/tf/landing_zone.tf
+++ spanner/finance-advisor/tf/landing_zone.tf
@@ -1,3 +1,79 @@
+#Provision Client VM
+resource "google_compute_instance" "database-clientvm" {
+  depends_on = [google_project_service.project_services]
+
+  name         = var.clientvm-name
+  machine_type = "e2-medium"
+  zone         = "${var.region}-${var.zone}"
+  project      = local.project_id
+
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240617"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.demo_network.id
+  }
+
+  service_account {
+    email  = "${local.project_number}-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+  }
+
+}
+
+#this is just to make sure we have ssh keys
+resource "null_resource" "lz-init-gcloud-ssh" {
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud compute config-ssh
+    EOT
+  }
+
+  depends_on = [google_project_service.project_services]
+}
+
+resource "time_sleep" "wait_for_database_clientvm_boot" {
+  create_duration = "120s" # Adjust the wait time based on your VM boot time
+
+  depends_on = [google_compute_instance.database-clientvm,
+  null_resource.lz-init-gcloud-ssh]
+}
+resource "null_resource" "enable_service_usage_api" {
+  provisioner "local-exec" {
+    command = "gcloud services enable serviceusage.googleapis.com --project ${local.project_id}"
+  }
+}
+
+#Enable APIs
+locals {
+  apis_to_enable = [
+    "compute.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "vpcaccess.googleapis.com",
+    "iam.googleapis.com",
+    "iap.googleapis.com",
+    "serviceusage.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "project_services" {
+  for_each           = toset(local.apis_to_enable)
+  service            = each.key
+  disable_on_destroy = false
+  depends_on         = [null_resource.enable_service_usage_api]
+  project            = local.project_id
+}
 #Use EXISTING project
 #----------------------------------------------------------
 resource "null_resource" "enable_service_usage_api_pre_proj" {
@@ -72,101 +148,6 @@ locals {
   project_name   = var.create_new_project ? google_project.demo-project[0].name : data.google_project.project_check[0].name
   project_number = var.create_new_project ? google_project.demo-project[0].number : data.google_project.project_check[0].number
 }
-resource "null_resource" "enable_service_usage_api" {
-  provisioner "local-exec" {
-    command = "gcloud services enable serviceusage.googleapis.com --project ${local.project_id}"
-  }
-}
-
-#Enable APIs
-locals {
-  apis_to_enable = [
-    "compute.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "servicenetworking.googleapis.com",
-    "vpcaccess.googleapis.com",
-    "iam.googleapis.com",
-    "iap.googleapis.com",
-    "serviceusage.googleapis.com"
-  ]
-}
-
-resource "google_project_service" "project_services" {
-  for_each           = toset(local.apis_to_enable)
-  service            = each.key
-  disable_on_destroy = false
-  depends_on         = [null_resource.enable_service_usage_api]
-  project            = local.project_id
-}
-#Add required roles to the default compute SA (used by clientVM and Cloud Build)
-locals {
-  default_compute_sa_roles_expanded = [
-    "roles/cloudbuild.builds.editor",
-    "roles/artifactregistry.admin",
-    "roles/storage.admin",
-    "roles/run.admin",
-    "roles/iam.serviceAccountUser",
-    "roles/aiplatform.user"
-  ]
-}
-
-resource "google_project_iam_member" "default_compute_sa_roles_expanded" {
-  for_each   = toset(local.default_compute_sa_roles_expanded)
-  project    = local.project_id
-  role       = each.key
-  member     = "serviceAccount:${local.project_number}-compute@developer.gserviceaccount.com"
-  depends_on = [time_sleep.wait_for_database_clientvm_boot] #30-clientvm.tf
-}
-#Provision Client VM
-resource "google_compute_instance" "database-clientvm" {
-  depends_on = [google_project_service.project_services]
-
-  name         = var.clientvm-name
-  machine_type = "e2-medium"
-  zone         = "${var.region}-${var.zone}"
-  project      = local.project_id
-
-
-  boot_disk {
-    initialize_params {
-      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240617"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.demo_network.id
-  }
-
-  service_account {
-    email  = "${local.project_number}-compute@developer.gserviceaccount.com"
-    scopes = ["cloud-platform"]
-  }
-
-  shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_secure_boot          = true
-    enable_vtpm                 = true
-  }
-
-}
-
-#this is just to make sure we have ssh keys
-resource "null_resource" "lz-init-gcloud-ssh" {
-  provisioner "local-exec" {
-    command = <<EOT
-      gcloud compute config-ssh
-    EOT
-  }
-
-  depends_on = [google_project_service.project_services]
-}
-
-resource "time_sleep" "wait_for_database_clientvm_boot" {
-  create_duration = "120s" # Adjust the wait time based on your VM boot time
-
-  depends_on = [google_compute_instance.database-clientvm,
-  null_resource.lz-init-gcloud-ssh]
-}
 # Network Resources
 resource "google_compute_network" "demo_network" {
   name                    = "demo-network"
@@ -234,6 +215,25 @@ resource "google_compute_router_nat" "nat-config" {
   project                            = local.project_id
 }
 
+#Add required roles to the default compute SA (used by clientVM and Cloud Build)
+locals {
+  default_compute_sa_roles_expanded = [
+    "roles/cloudbuild.builds.editor",
+    "roles/artifactregistry.admin",
+    "roles/storage.admin",
+    "roles/run.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/aiplatform.user"
+  ]
+}
+
+resource "google_project_iam_member" "default_compute_sa_roles_expanded" {
+  for_each   = toset(local.default_compute_sa_roles_expanded)
+  project    = local.project_id
+  role       = each.key
+  member     = "serviceAccount:${local.project_number}-compute@developer.gserviceaccount.com"
+  depends_on = [time_sleep.wait_for_database_clientvm_boot] #30-clientvm.tf
+}
 
 resource "google_project_service" "lz_dataflow_service" {
   service    = "dataflow.googleapis.com"
