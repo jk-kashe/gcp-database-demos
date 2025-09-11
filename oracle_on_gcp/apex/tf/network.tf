c4a8fc01c825bf19d1ea4ab497b305cde44c1351
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-09-11 15:07:15 +0000

    wip

--- oracle_on_gcp/apex/tf/network.tf
+++ oracle_on_gcp/apex/tf/network.tf
@@ -26,6 +26,21 @@ resource "google_compute_firewall" "oracle_allow_iap" {
   }
 }
 
+# Create a NAT gateway to allow outbound internet access for private VMs
+resource "google_compute_router" "nat_router" {
+  name    = "nat-router"
+  region  = var.region
+  network = google_compute_network.oracle.name
+}
+
+resource "google_compute_router_nat" "nat_gateway" {
+  name                               = "nat-gateway"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
 output "network_name" {
   value = google_compute_network.oracle.name
 }
