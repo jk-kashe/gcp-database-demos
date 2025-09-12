86cfa4f081726599f54591e21d0bccdc03bb9d13
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-09-12 09:14:53 +0000

    fix(autovars): ensure a valid region is always selected

--- .blocks/utils/auto-vars.sh
+++ .blocks/utils/auto-vars.sh
@@ -2,47 +2,20 @@
 
 # Function to get a valid alloydb trial region
 get_valid_region() {
-  local current_region=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
-  echo "$current_region"
-  return
-  ##old code is now unreachable
-  # If region is already valid, return it
-  case "$current_region" in
-    us-central1 | northamerica-northeast1 | asia-east1 | asia-northeast2 | asia-south2 | asia-southeast1 | australia-southeast2 | europe-north1 | europe-west1 | europe-west4)
-      echo "$current_region"
-      return
-      ;;
-  esac
+  # Try to get the default region from gcloud config
+  local current_region=$(gcloud config get-value compute/region 2>/dev/null)
 
-  # If region is invalid, try to match the prefix
-  case "$current_region" in
-    us-*)
-      echo "us-central1"
-      ;;
-    northamerica-*)
-      echo "northamerica-northeast1"
-      ;;
-    asia-*)
-      # Pick a random "asia-" region
-      local asia_regions=(asia-east1 asia-northeast2 asia-south2 asia-southeast1)
-      echo "${asia_regions[$((RANDOM % ${#asia_regions[@]}))]}"
-      ;;
-    europe-*)
-      # Pick a random "europe-" region
-      local europe_regions=(europe-north1 europe-west1 europe-west4)
-      echo "${europe_regions[$((RANDOM % ${#europe_regions[@]}))]}"
-      ;;
-    australia-*)
-      echo "australia-southeast2"
-      ;;
-    *)
-      echo "europe-west1"
-      # below idea has issues on quicklabs, defaulting to us-central1 now
-      # If no region is set or prefix doesn't match, pick a completely random region
-      #local all_regions=(us-central1 northamerica-northeast1 asia-east1 asia-northeast2 asia-south2 asia-southeast1 australia-southeast2 europe-north1 europe-west1 europe-west4)
-      #echo "${all_regions[$((RANDOM % ${#all_regions[@]}))]}"
-      ;;
-  esac
+  # If the region is not set in the config, get it from the project metadata
+  if [ -z "$current_region" ]; then
+    current_region=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])" 2>/dev/null)
+  fi
+
+  # If region is still not set, default to a valid one
+  if [ -z "$current_region" ]; then
+    current_region="europe-west1"
+  fi
+
+  echo "$current_region"
 }
 
 
