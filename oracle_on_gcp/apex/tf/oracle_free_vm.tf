63cf1b86abadce403803fad95490aabaffcef576
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-09-24 13:40:15 +0000

    feat: Add APEX setup and update README

--- oracle_on_gcp/apex/tf/oracle_free_vm.tf
+++ oracle_on_gcp/apex/tf/oracle_free_vm.tf
@@ -9,11 +9,24 @@ resource "google_compute_firewall" "allow_oracle_vm" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+# Create a firewall rule to allow access to APEX
+resource "google_compute_firewall" "allow_apex_vm" {
+  name    = "allow-apex-vm"
+  network = google_compute_network.oracle.name
+  allow {
+    protocol = "tcp"
+    ports    = ["8181"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["apex-vm"]
+}
+
 # Create a Compute Engine instance with OS Login enabled
 resource "google_compute_instance" "oracle_vm" {
   name         = "oracle-vm"
   machine_type = var.vm_machine_type
   zone         = data.google_compute_zones.available.names[0] # Select the first available zone
+  tags         = ["apex-vm"]
 
   boot_disk {
     initialize_params {
@@ -43,7 +56,7 @@ resource "google_compute_instance" "oracle_vm" {
 # Wait for the instance to be fully ready for SSH
 resource "time_sleep" "wait_for_vm_ssh" {
   create_duration = "90s"
-  depends_on      = [google_compute_instance.oracle_vm]
+  depends_on      = [google_compute_instance.oracle_.vm]
 }
 
 # Provision the VM after the delay
@@ -52,7 +65,18 @@ resource "null_resource" "provision_oracle_vm" {
 
   provisioner "local-exec" {
     command = <<EOT
-      gcloud compute ssh ${google_compute_instance.oracle_vm.name} --zone=${google_compute_instance.oracle_vm.zone} --project=${var.project_id} --tunnel-through-iap --command='sudo apt-get update && sudo apt-get install -y docker.io && sudo docker run -d -p 1521:1521 -e ORACLE_PASSWORD=${var.vm_oracle_password} gvenzl/oracle-free:latest'
+      gcloud compute ssh ${google_compute_instance.oracle_vm.name} --zone=${google_compute_instance.oracle_vm.zone} --project=${var.project_id} --tunnel-through-iap --command='sudo apt-get update && sudo apt-get install -y docker.io && sudo docker run -d --name oracle-free -p 1521:1521 -e ORACLE_PASSWORD=${var.vm_oracle_password} gvenzl/oracle-free:latest'
+    EOT
+  }
+}
+
+# Provision the APEX container
+resource "null_resource" "provision_apex_vm" {
+  depends_on = [null_resource.provision_oracle_vm]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud compute ssh ${google_compute_instance.oracle_vm.name} --zone=${google_compute_instance.oracle_vm.zone} --project=${var.project_id} --tunnel-through-iap --command='sudo docker run -d --name ords -p 8181:8181 --restart always --network=container:oracle-free container-registry.oracle.com/database/ords-developer:24.4.0'
     EOT
   }
 }
