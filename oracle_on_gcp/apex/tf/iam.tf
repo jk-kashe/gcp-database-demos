1a8bff6233e66b777e1770dd7e3e8589cde51fea
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-09-24 16:03:11 +0000

    fix: Grant Storage Object Admin to Compute SA on build bucket

--- oracle_on_gcp/apex/tf/iam.tf
+++ oracle_on_gcp/apex/tf/iam.tf
@@ -1,10 +1,10 @@
 data "google_project" "project" {}
 
-# Grant the Cloud Build service account access to the GCS bucket
-resource "google_storage_bucket_iam_member" "cloudbuild_gcs_access" {
+# Grant the Compute Engine service account access to the GCS bucket
+resource "google_storage_bucket_iam_member" "compute_gcs_access" {
   bucket = "${var.project_id}_cloudbuild"
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
 
 # Grant the Cloud Build service account access to Artifact Registry
