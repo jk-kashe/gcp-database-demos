65a8c0235895f3283ee851b709c8ad1881897383
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-15 17:26:15 +0000

    refactor: modularize oracle on gcp apex deployment
    Refactored the monolithic Terraform configuration for deploying Oracle APEX on Google Cloud into a modular structure.
    
    - Created reusable modules for the landing zone, the free-tier Oracle database, and the ORDS Cloud Run service.
    - The new structure is more maintainable and extensible.
    - The client VM has been removed in this refactoring.

---
+++ modules/oracle/cloud-run-ords/variables.tf
@@ -0,0 +1,47 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID to deploy to."
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy to."
+}
+
+variable "apis" {
+  type        = list(string)
+  description = "The APIs to enable."
+  default = [
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+    "cloudbuild.googleapis.com"
+  ]
+}
+
+variable "vm_oracle_password" {
+  type        = string
+  description = "The password for the Oracle database."
+  sensitive   = true
+}
+
+variable "oracle_db_ip" {
+  type        = string
+  description = "The IP address of the Oracle database."
+}
+
+variable "vpc_connector_id" {
+  type        = string
+  description = "The ID of the VPC connector."
+}
+
+variable "db_instance_dependency" {
+  description = "This is not a real variable, just a way to enforce dependencies.  This should be a comma separated list of resources that need to be created before this module is called."
+  type        = any
+  default     = null
+}
+
+variable "iam_dependency" {
+  description = "This is not a real variable, just a way to enforce dependencies.  This should be a comma separated list of resources that need to be created before this module is called."
+  type        = any
+  default     = null
+}
