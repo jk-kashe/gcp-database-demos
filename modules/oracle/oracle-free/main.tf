e3db1a6d8d3467887fd179ae3d4031318c85e7c5
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-17 15:34:04 +0000

    apex install - removing output redirect

--- modules/oracle/oracle-free/main.tf
+++ modules/oracle/oracle-free/main.tf
@@ -41,6 +41,8 @@ resource "google_compute_instance" "oracle_vm" {
       curl -o /tmp/unattended_apex_install_23c.sh https://raw.githubusercontent.com/Pretius/pretius-23cfree-unattended-apex-installer/main/src/unattended_apex_install_23c.sh
       curl -o /tmp/00_start_apex_ords_installer.sh https://raw.githubusercontent.com/Pretius/pretius-23cfree-unattended-apex-installer/main/src/00_start_apex_ords_installer.sh
       
+      sudo sed -i 's| > /home/oracle/unattended_apex_install_23c.log||' /tmp/00_start_apex_ords_installer.sh
+
       sudo sed -i "s/OrclAPEX1999!/${random_password.apex_admin_password.result}/g" /tmp/unattended_apex_install_23c.sh
       sudo sed -i "s/ALTER USER APEX_PUBLIC_USER IDENTIFIED BY E;/ALTER USER APEX_PUBLIC_USER IDENTIFIED BY ${random_password.db_user_password.result};/g" /tmp/unattended_apex_install_23c.sh
       sudo sed -i "/<<EOT/,/EOT/ s/^E$/${random_password.db_user_password.result}/" /tmp/unattended_apex_install_23c.sh
