32b7b0af76b003844dd9f9d671dc64dec330252b
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-18 19:16:02 +0000

    another toaster error

--- oracle_on_gcp/apex/tf/main.tf
+++ oracle_on_gcp/apex/tf/main.tf
@@ -44,7 +44,7 @@ module "landing_zone" {
   region                  = var.region
   zone                    = random_shuffle.zone.result[0]
   provision_vpc_connector = true
-  additional_apis         = ["secretmanager.googleapis.com", "cloudbuild.googleapis.com", "servicedirectory.googleapis.com"]
+  additional_apis         = ["secretmanager.googleapis.com", "cloudbuild.googleapis.com", "servicedirectory.googleapis.com", "dns.googleapis.com"]
 }
 
 module "oracle_free" {
