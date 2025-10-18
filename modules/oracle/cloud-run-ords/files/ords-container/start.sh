894a1e3cbf1f0f9d7eb68850b734c6d240c52978
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-18 08:58:33 +0000

    ords

--- modules/oracle/cloud-run-ords/files/ords-container/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Write the connection string from the environment variable to the file
-echo "CONN_STRING=${CONN_STRING}" > /opt/oracle/variables/conn_string.txt
-
-# Start the ORDS service, listening on the port specified by the PORT env var
-/opt/oracle/ords/bin/ords --config /etc/ords/config serve --port ${PORT}
