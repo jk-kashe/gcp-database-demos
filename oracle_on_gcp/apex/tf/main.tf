2433417b81b562ef72749f3fbd78e43323e191e6
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-18 11:19:46 +0000

    toaster bug

--- oracle_on_gcp/apex/tf/main.tf
+++ oracle_on_gcp/apex/tf/main.tf
@@ -22,10 +22,6 @@ resource "google_storage_bucket" "ords_config" {
   uniform_bucket_level_access = true
 }
 
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
 resource "google_storage_bucket_iam_member" "compute_sa_gcs_access" {
   bucket = google_storage_bucket.ords_config.name
   role   = "roles/storage.objectAdmin"
