66228db63280ff007bb5bdf6a313f0b173f70c5f
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-10-18 13:15:06 +0000

    disable api removal

--- modules/oracle/cloud-run-ords/main.tf
+++ modules/oracle/cloud-run-ords/main.tf
@@ -8,6 +8,7 @@ resource "google_project_service" "api" {
   ])
   project                    = var.project_id
   service                    = each.key
+  disable_on_destroy         = false
   disable_dependent_services = true
 }
 
