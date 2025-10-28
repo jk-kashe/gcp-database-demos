600c34cc34b45162521886442e502726713d497b
J.K. Kashe <127425778+jk-kashe@users.noreply.github.cooud>
2025-10-28 06:59:44 +0000

    Added list schema object to tolls

--- modules/oracle/mcp-toolbox-oracle/main.tf
+++ modules/oracle/mcp-toolbox-oracle/main.tf
@@ -23,9 +23,59 @@ locals {
         source: my-oracle-source
         description: "Executes an arbitrary SQL statement against the Oracle database."
 
+      schema-list-tables:
+        kind: oracle-sql
+        source: my-oracle-source
+        description: "Lists  all tables in a given Oracle schema"
+        statement: |
+            SELECT table_name FROM ALL_TAB_COLUMNS where owner = :1
+        parameters:
+          - name: schema_name
+            type: string
+            description: "The name of the Oracle schema to inspect - case sensitive (all caps by default)."
+
+      schema-list-columns:
+        kind: oracle-sql
+        source: my-oracle-source
+        description: "Lists all columns for all tables in a given Oracle schema"
+        statement: |
+            SELECT table_name, column_name, data_type FROM ALL_TAB_COLUMNS where owner = :1
+        parameters:
+          - name: schema_name
+            type: string
+            description: "The name of the Oracle schema to inspect - case sensitive (all caps by default)."
+
+      schema-list-fk:
+        kind: oracle-sql
+        source: my-oracle-source
+        description: "Lists all foreign keys for all tables in a given Oracle schema"
+        statement: |
+            SELECT 
+              a.table_name, 
+              a.column_name, 
+              c.table_name AS referenced_table_name, 
+              c.column_name AS referenced_column_name,
+              a.constraint_name 
+            FROM ALL_CONS_COLUMNS a 
+            JOIN ALL_CONSTRAINTS b ON a.owner = b.owner 
+                                   AND a.constraint_name = b.constraint_name
+            JOIN ALL_CONS_COLUMNS c ON b.owner = c.owner 
+                                   AND b.r_constraint_name = c.constraint_name 
+            WHERE 
+              b.constraint_type = 'R' AND
+              a.owner = :1 
+            ORDER BY a.table_name, a.constraint_name, a.position
+        parameters:
+          - name: schema_name
+            type: string
+            description: "The name of the Oracle schema to inspect - case sensitive (all caps by default)."
+
     toolsets:
       oracle-tools:
         - execute-ad-hoc-oracle-sql
+        - schema-list-tables
+        - schema-list-columns
+        - schema-list-fk
   EOT
 }
 
